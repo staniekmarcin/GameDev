074abc948933e8c1d9a77dab3e16830dafcf7f75
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-11 13:28:32 +0200

    A better component architecture
    *Movement Component in different way

--- BattleTank/Source/Private/Tank.cpp
+++ BattleTank/Source/Private/Tank.cpp
@@ -15,7 +15,7 @@ ATank::ATank()
 
 	// No need to protect pointers as added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
+	//TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 void ATank::SetBarrelReference(UTankBarrel *BarrelToSet)
 {
--- BattleTank/Source/Private/TankMovementComponent.cpp
+++ BattleTank/Source/Private/TankMovementComponent.cpp
@@ -1,9 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    if(!LeftTrackToSet || !RightTrackToSet)
+    {
+        LeftTrack = LeftTrackToSet;
+        RightTrack = RightTrackToSet;
+    }
+}
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
     //auto Name = GetName();
     UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+    // TODO prevet double speed
+    
 }
