7f5105bfa5ba075073e5f05357abf84d5b341f89
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-20 14:24:46 +0200

    Destroying Projectiles with GetTimerManager()
    *Protects from overloading WorldOutliner

--- 04_BattleTank/BattleTank/Source/Private/Projectile.cpp
+++ 04_BattleTank/BattleTank/Source/Private/Projectile.cpp
@@ -55,4 +55,14 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 	ImpactBlast->Activate();
 	ExplosionForce->FireImpulse();
 	
+	SetRootComponent(ImpactBlast);
+	CollisionMesh->DestroyComponent();
+
+	FTimerHandle Timer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
+}
+
+void AProjectile::OnTimerExpire()
+{
+	Destroy();
 } 	
