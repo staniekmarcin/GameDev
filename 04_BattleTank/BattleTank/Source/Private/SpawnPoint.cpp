89f41bbf633def8f2cbc12d312332424348f84c4
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-23 14:46:02 +0200

    SpawnActorDeferred and BeginPlay

--- 04_BattleTank/BattleTank/Source/Private/SpawnPoint.cpp
+++ 04_BattleTank/BattleTank/Source/Private/SpawnPoint.cpp
@@ -19,9 +19,10 @@ void USpawnPoint::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto NewActor = GetWorld()->SpawnActor<AActor>(SpawnClass);
+	auto NewActor = GetWorld()->SpawnActorDeferred<AActor>(SpawnClass, GetComponentTransform());
 	if (!NewActor) return;
-	NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepRelativeTransform);
+	NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+	UGameplayStatics::FinishSpawningActor(NewActor, GetComponentTransform());
 }
 
 // Called every frame
