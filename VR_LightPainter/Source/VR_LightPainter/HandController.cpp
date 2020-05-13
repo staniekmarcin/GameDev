b90d1ea570ee68a4891c5ec3e9096f09f2fc4437
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-13 14:35:10 +0200

    Classes for VRPawn and HandController

---
+++ VR_LightPainter/Source/VR_LightPainter/HandController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HandController.h"
+
+// Sets default values
+AHandController::AHandController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionController"));
+	SetRootComponent(MotionController);
+	MotionController->SetTrackingSource(EControllerHand::Right);
+	MotionController->SetShowDeviceModel(true);
+}
+
+void AHandController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AHandController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ VR_LightPainter/Source/VR_LightPainter/VRPawn.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRPawn.h"
+
+#include "Engine/World.h"
+
+#include "HandController.h"
+
+// Sets default values
+AVRPawn::AVRPawn()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	SetRootComponent(VRRoot);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(VRRoot);
+}
+
+void AVRPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HandControllerClass)
+	{
+		RightHandController = GetWorld()->SpawnActor<AHandController>(HandControllerClass);
+		RightHandController->AttachToComponent(GetRootComponent(), FAttachmentTransformRules::SnapToTargetIncludingScale);
+		RightHandController->SetOwner(this);
+	}
+
+
+	
+	
+}
