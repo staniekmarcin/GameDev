6c0b780b1b456b7db4316a60bd27c1ddbaa5409c
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-18 18:30:27 +0200

    Button Actions And OpenLevel

--- VR_LightPainter/Source/VR_LightPainter/UI/PaintingPicker/PaintingGridCard.cpp
+++ VR_LightPainter/Source/VR_LightPainter/UI/PaintingPicker/PaintingGridCard.cpp
@@ -3,7 +3,18 @@
 
 #include "PaintingGridCard.h"
 
-void UPaintingGridCard::SetPaintingName(FString PaintingName)
+#include "Kismet/GameplayStatics.h"
+
+void UPaintingGridCard::SetPaintingName(FString NewPaintingName)
 {
+	PaintingName = NewPaintingName;
+
 	SlotName->SetText(FText::FromString(PaintingName));
+
+	CardButton->OnClicked.AddDynamic(this, &UPaintingGridCard::CardButtonClicked);
+}
+
+void UPaintingGridCard::CardButtonClicked()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), TEXT("TestMap"), true, "SlotName = " + PaintingName );
 }
