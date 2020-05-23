2e45290201892e26b6177e9de5715669ea7c6139
Marcin Staniek <marcin.staniek97@gmail.com>
2020-05-23 14:15:26 +0200

    Gun Actors

---
+++ 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/Gun.cpp
@@ -0,0 +1,29 @@
+
+
+#include "Gun.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+AGun::AGun()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);	
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Root);
+}
+
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ 05_SimpleShooter/SimpleShooter/Source/SimpleShooter/Gun.h
@@ -0,0 +1,29 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+UCLASS()
+class SIMPLESHOOTER_API AGun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AGun();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* Root;
+
+	UPROPERTY(VisibleAnywhere)
+	USkeletalMeshComponent* Mesh;
+};
