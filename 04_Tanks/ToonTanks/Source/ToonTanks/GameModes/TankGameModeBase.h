143fff9a5e9950d7f4f069fb7d68e478dfca3215
Marcin Staniek <marcin.staniek97@gmail.com>
2020-06-19 11:58:51 +0200

    GameMode classes

---
+++ 04_Tanks/ToonTanks/Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+
+    virtual void BeginPlay() override;
+
+public:
+
+    void ActorDied(AActor* DeadActor);
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void GameStart();
+ 
+    UFUNCTION(BlueprintImplementableEvent)
+    void GameOver();
+
+    
+private:
+    void HandleGameStart();
+    void HandleGameOver(bool PlayerWon);
+};
